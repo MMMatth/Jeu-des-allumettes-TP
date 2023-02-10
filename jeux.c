d8e6de6716a9e98abb5daea565f544fb718b5cdf
MMMatth <mat.loup04@gmail.com>
2023-02-10 14:38:23 +0100

    ajout d'un readme.md

--- jeux.c
+++ jeux.c
@@ -8,12 +8,9 @@
 
 int main(int argc, char * argv[]){
     int nb_batton = 12;
-
     tab_t *jeux = make(nb_batton);
     int play = 1;
-
     fill_jeu(jeux);
-
     printf("----- Debut de la partie ----- \n");
     while (play){
         manche(jeux, 1);
@@ -21,14 +18,5 @@ int main(int argc, char * argv[]){
         manche(jeux, 2);
         if (qui_gagne(jeux) == 1 || qui_gagne(jeux) == 0){break;}
     }
-    
-
-
-
-
-
-
-
-
     return EXIT_SUCCESS;
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Truc a faire :
+- [ ] Utiliser le argv
+- [ ] transformer tableau 0 1 et tableau 1 2
